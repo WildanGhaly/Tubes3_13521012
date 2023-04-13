bb763d0d0f2828ada8be388f825f3a31dfce5478
Hidayatullah Wildan Ghaly Buchary <16521502@std.stei.itb.ac.id>
2023-04-13 14:35:45 +0700

    feat: add template frontend - program is now looks like chatGPT - program can now input text - program can now do console log to inputted text - program can now clear the text after sending the input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Button(props) {
+    return (
+        <button className="my-button" onClick={props.onClick}>
+            {props.label}
+        </button>
+    );
 }
+
+function App() {
+    const handleSend = () => {
+        const userInput = document.getElementById("my-input").value;
+        console.log(userInput);
+        document.getElementById("my-input").value = "";
+    };
+
+    return (
+        <div className="container">
+            <div className="sidebar">
+                <button className="my-button"> + New Chat </button>
+            </div>
+            <div className="content">
+                <div className="user-text-input">
+                    <input type="text" id="my-input" placeholder="Enter your text here" />
+                    <label htmlFor="my-input" style={{ display: "none" }}>
+                        Enter your text here
+                    </label>
+                    <button onClick={handleSend}>Send</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
 
 export default App;
