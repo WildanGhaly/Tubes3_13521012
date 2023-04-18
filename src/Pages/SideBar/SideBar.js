ac54ebd5b541da7961ebc1464ced1fee14dffc3a
Hidayatullah Wildan Ghaly Buchary <16521502@std.stei.itb.ac.id>
2023-04-18 15:47:26 +0700

    feat: add new chat mechanism - Program can now add button after new chat on click - The sidebar is now scrollable - Fixed bug when sidebar overflow

--- src/Pages/SideBar/SideBar.js
+++ src/Pages/SideBar/SideBar.js
@@ -1,15 +1,29 @@
+import { useState } from 'react';
 import './SideBar.css';
 import Radio from '../../Components/Radio.js';
 
 function SideBar() {
-    return (
-        <div className="sidebar">
-            <div className="sidebar-content">
-                <button className="my-button"> + New Chat </button>
-            </div>
-            <Radio></Radio>
-        </div>
-    );
+  const [buttonCount, setButtonCount] = useState(0);
+
+  const handleNewButton = () => {
+    setButtonCount(buttonCount + 1);
+  };
+
+  const newButtons = [];
+
+  for (let i = 0; i < buttonCount; i++) {
+    newButtons.push(<button key={i} className="my-button">Button {i + 1}</button>);
+  }
+
+  return (
+    <div className="sidebar">
+      <div className="sidebar-content">
+        <button className="my-button" onClick={handleNewButton}> + New Chat </button>
+        {newButtons}
+      </div>
+      <Radio />
+    </div>
+  );
 }
 
 export default SideBar;
