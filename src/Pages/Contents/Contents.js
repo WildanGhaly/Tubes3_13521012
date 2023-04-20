e070c732f77268e15e649f536a1108e352db11ca
Hidayatullah Wildan Ghaly Buchary <16521502@std.stei.itb.ac.id>
2023-04-20 21:22:04 +0700

    feat: add appendable user text - Program can now show user's multiple input - Program will show user input to content after sending one or more text

--- src/Pages/Contents/Contents.js
+++ src/Pages/Contents/Contents.js
@@ -1,18 +1,22 @@
-import { useState } from 'react';
+import { useState } from "react";
 import TextInput from '../../Components/TextInput';
 import './Contents.css';
 
 function Contents() {
-  const [userInput, setUserInput] = useState('');
+  const [messages, setMessages] = useState([]);
 
-  function handleUserInput(input) {
-    setUserInput(input);
+  function handleSend(message) {
+    setMessages([...messages, message]);
   }
 
   return (
     <div className="content">
-      <TextInput onSend={handleUserInput} />
-      <p>{userInput}</p>
+      <TextInput onSend={handleSend} />
+      <div className="messages">
+        {messages.map((message, index) => (
+          <div key={index}>{message}</div>
+        ))}
+      </div>
     </div>
   );
 }
