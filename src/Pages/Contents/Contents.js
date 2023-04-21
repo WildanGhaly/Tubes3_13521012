eec81bcb7fd0c4e4920a265d2e140251de7313a9
Hidayatullah Wildan Ghaly Buchary <16521502@std.stei.itb.ac.id>
2023-04-21 23:31:51 +0700

    feat: add bot response mechanism - the program can now response to user input - user input can now be parse to backend - backend is not yet build

--- src/Pages/Contents/Contents.js
+++ src/Pages/Contents/Contents.js
@@ -6,17 +6,26 @@ function Contents() {
   const [messages, setMessages] = useState([]);
 
   function handleSend(message) {
-    setMessages([...messages, message]);
+    setMessages([...messages, { text: message, isUser: true }]);
+    /* Call backend here */
+    setMessages([...messages, { text: message, isUser: true }, { text: "Ya Ndak Tahu", isUser: false }]);
   }
 
   return (
     <div className="content">
       <TextInput onSend={handleSend} />
-      <div className="messages">
-        {messages.map((message, index) => (
-          <div key={index}>{message}</div>
-        ))}
-      </div>
+      {messages.length > 0 && (
+        <div className="messages">
+          {messages.map((message, index) => (
+            <div
+              key={index}
+              className={`message ${message.isUser ? "user-message" : "bot-message"}`}
+            >
+              {message.text}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
