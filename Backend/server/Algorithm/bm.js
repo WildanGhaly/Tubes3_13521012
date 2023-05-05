5385b6271de1c225b1932e716e769452d113659d
Haidar <16521522@mahasiswa.itb.ac.id>
2023-05-05 16:45:45 +0700

    feat: add similarity 90% to bm algorithm

--- Backend/server/Algorithm/bm.js
+++ Backend/server/Algorithm/bm.js
@@ -1,5 +1,6 @@
 const { con, getQuestions, getAnswers } = require("../db");
 const { calculateEquation } = require("./calculator");
+const { getSimilarity, levenshteinDistance } = require("./similarity");
 /**
  * @param {string} text
  * Pencarian pertanyaan yang paling mirip dengan pertanyaan yang diberikan pengguna dilakukan dengan 
@@ -11,9 +12,53 @@ const { calculateEquation } = require("./calculator");
 function bm (text) {
     console.log("BM");
     console.log(text);
+    const addQuestionRegex = /^tambahkan pertanyaan\s*(.+)\s*dengan jawaban\s*(.+)\s*$/i;
+    const removeQuestionRegex = /^hapus pertanyaan\s*(.+)\s*$/i;
+    const mathExpressionRegex = /([\+\-\*\/\^\\%\|\&\(\)])/;
+    const dateExpressionRegex = /^((0[1-9]|[1-2][0-9]|3[0-1])-(0[1-9]|1[0-2])-\d{4})|((0[1-9]|[1-2][0-9]|3[0-1])\/(0[1-9]|1[0-2])\/\d{4})|(\d{4}\/(0[1-9]|[1-2][0-9]|3[0-1])\/(0[1-9]|1[0-2]))|(\d{4}-(0[1-9]|[1-2][0-9]|3[0-1])-(0[1-9]|1[0-2]))$/
+                                   /*  dd-mm-yyyy  */                                 /*  dd/mm/yyyy  */                                   /* yyyy/mm/dd */                                    /* yyyy-mm-dd */
+
+    if (addQuestionRegex.test(text)) {
+        return new Promise(function(resolve, reject) {
+            const matches = text.match(addQuestionRegex);
+            const question = matches[1].toLowerCase().trim();
+            const answer = matches[2];
+            getAnswers(question).then(function(result){
+                if (result.length == 0){
+                    insertQuestions(question, answer);
+                    resolve("Pertanyaan " + question + " telah ditambahkan (KMP)" );
+                } else {
+                    updateAnswer(question, answer);
+                    resolve("Pertanyaan " + question + " sudah ada! jawaban diupdate ke " + answer);
+                }
+            });
+        });
+    } else if (removeQuestionRegex.test(text)) {
+        return new Promise(function(resolve, reject) {
+            const matches = text.match(removeQuestionRegex);
+            const question = matches[1].toLowerCase();
+            getAnswers(question).then(function(result){
+                if (result.length == 0){
+                    resolve("Tidak ada pertanyaan " + question + " pada database!");
+                } else {
+                    deleteQuestion(question);
+                    resolve("Pertanyaan " + question + " telah dihapus");
+                }
+            }); 
+        });
+    } else if (dateExpressionRegex.test(text)){
+        return new Promise(function(resolve, reject) {
+            resolve(getDayOfWeek(text));
+        });
+    } else if (mathExpressionRegex.test(text)){
+        return new Promise(function(resolve, reject) {
+            resolve(calculateEquation(text));
+        });
+    } else {
     return new Promise(function(resolve, reject) {
         var validation = -1;
         var qResult = [];
+        const final_question = [];
         var i = -1;
         var result;
 
@@ -32,17 +77,51 @@ function bm (text) {
                     } while (validation == -1);
 
                     if (validation == -1){
-                        result = 'Maaf jawaban dari pertanyaan belum ada di database :( (BM) '; 
-                        resolve(result); // resolve the result here
+                        // const similarStrings = 
+                        getSimilarity(text).then(function(result) {
+                        const similarStrings = result;
+                        // console.log(similarStrings);
+                        // console.log(similarStrings.length);
+                        // console.log("test");
+                        // console.log(similarStrings[0]);
+                        if (similarStrings.length == 0){
+                            result = 'Maaf jawaban dari pertanyaan belum ada di database :( (BM) '; 
+                            resolve(result); // resolve the result here
+                        } else {
+                            if (similarStrings[0].similarity >= 0.9) {
+                                getAnswers(similarStrings[0].string)
+                                    .then(function(result) {
+                                        result = result[0].answer;
+                                        // result =
+                                        // // result == "DATE" ? new Date().toLocaleDateString() : 
+                                        // // result == "TIME" ? new Date().toLocaleTimeString() : 
+                                        // // result == "CALC" ? calculateEquation(text) :
+                                        // result;
+                                        console.log(result + " (BM)");
+                                        resolve(result); // resolve the result here
+                                });
+                            }
+                            else {
+                            result = 'Apakah yang anda maksud adalah : ';
+                            for (let i = 0; i < 3; i++) {
+                                result += "\n" + (i + 1) + ". " + similarStrings[i].string;
+                            } 
+                            resolve(result); // resolve the result here
+                        }
+                            
+                        }
+
+                        });
+
                     } else {
                         getAnswers(qResult[i].question)
                             .then(function(result) {
                                 result = result[0].answer;
-                                result =
-                                result == "DATE" ? new Date().toLocaleDateString() : 
-                                result == "TIME" ? new Date().toLocaleTimeString() : 
-                                result == "CALC" ? calculateEquation(text) :
-                                result;
+                                // result =
+                                // result == "DATE" ? new Date().toLocaleDateString() : 
+                                // result == "TIME" ? new Date().toLocaleTimeString() : 
+                                // result == "CALC" ? calculateEquation(text) :
+                                // result;
                                 console.log(result + " (BM)");
                                 resolve(result); // resolve the result here
                         });
@@ -53,8 +132,9 @@ function bm (text) {
         });
     });
 }
+}
     
-function bmMatch(question, pattern){
+function bmMatch(pattern, question){
     let m = pattern.length;
     let n = question.length;
 
@@ -120,7 +200,6 @@ for (let i = p, j = 0; i < m; i++, j++) {
 }
 return true;
 }
-
 
 
 module.exports = {bm};
